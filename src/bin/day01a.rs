80a7bff16ceb6ae9171d13ea2428262fdedc83ba
Nathan Chin <nathanchin@nathanchin.com>
2024-11-14 19:07:27 -0500

    Even even terser

--- src/bin/day01a.rs
+++ src/bin/day01a.rs
@@ -1,30 +1,30 @@
 use std::io::{stdin, Read};
 
-use color_eyre::eyre::{eyre, Result};
+use color_eyre::eyre::{OptionExt as _, Report, Result};
+use regex::Regex;
 
 fn main() -> Result<()> {
     let mut input = String::new();
     stdin().read_to_string(&mut input)?;
 
-    let r = regex::Regex::new(r#"(?<left>\d).*(?<right>\d)|(?<only>\d)"#)?;
+    let regex = Regex::new(r#"(?<left>\d).*(?<right>\d)|(?<only>\d)"#)?;
 
-    let mut sum: u128 = 0;
+    let ans = input.lines().try_fold(0, |sum, line| {
+        let caps = regex.captures(line).ok_or_eyre("Parsing error")?;
 
-    for line in input.lines() {
-        let caps = r.captures(line).ok_or(eyre!("Parsing error"))?;
+        Ok::<u128, Report>(
+            sum + if let Some(only) = caps.name("only") {
+                only.as_str().repeat(2)
+            } else if let (Some(left), Some(right)) = (caps.name("left"), caps.name("right")) {
+                left.as_str().to_owned() + right.as_str()
+            } else {
+                unreachable!()
+            }
+            .parse::<u128>()?,
+        )
+    })?;
 
-        sum += if let Some(only) = caps.name("only") {
-            only.as_str().repeat(2)
-        } else if let (Some(left), Some(right)) = (caps.name("left"), caps.name("right")) {
-            left.as_str().to_owned() + right.as_str()
-        } else {
-            unreachable!()
-        }
-        .parse::<u128>()
-        .map_err(|_| eyre!("integer parsing error"))?;
-    }
-
-    println!("{}", sum);
+    println!("{}", ans);
 
     Ok(())
 }
